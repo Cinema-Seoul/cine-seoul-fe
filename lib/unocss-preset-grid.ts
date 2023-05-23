00377d039111a05fd998c24b5843177f75e88bcb
Hogyun Jeon <akzm9999@gmail.com>
2023-05-23 14:09:51 +0900

    chore: Unocss, Styling 설정

--- lib/unocss-preset-grid.ts
+++ lib/unocss-preset-grid.ts
@@ -1,51 +1,62 @@
 /* eslint-disable no-empty-pattern */
-import type { Preset } from "unocss"
-import { escapeSelector } from "unocss"
+import type { Preset } from "unocss";
+import { escapeSelector } from "unocss";
 
 export interface GridOptions {
-  prefix?: string
-  piece?: string
-  gutter?: string
-  columns?: number
-  breakpoints?: Record<string, string>
+  prefix?: string;
+  piece?: string;
+  gutter?: string;
+  columns?: number;
+  breakpoints?: Record<string, string>;
 }
 
-export function presetGrid(options: GridOptions = {}): Preset {
-  // Default Columns
-  const columns = options?.columns ?? 12
+export function presetGrid(options: GridOptions = {}): Preset<any> {
 
-  // Default Prefix
-  const prefix = options?.prefix ?? "statu-"
+  const columns = options?.columns ?? 12;
+
+  const prefix = options?.prefix ?? 'cq-';
 
   // Default Breakpoints
-  const piece = options?.piece ?? "60px"
+  const piece = options?.piece ?? "60px";
   const breakpoints = options?.breakpoints ?? {
     sm: "640px",
     md: "768px",
     lg: "1024px",
     xl: "1280px",
-    "2xl": "1536px"
-  }
+    "2xl": "1536px",
+  };
 
   // Default Gutter
-  const gutter = options?.gutter ?? "1.5rem"
+  const gutter = options?.gutter ?? "1.5rem";
 
   return {
     name: "unocss-preset-grid",
+    variants: [
+      (matcher) => {
+        if (/\\@child$/.test(matcher)) {
+          return {
+            matcher: matcher.replace(/\\@child$/, ''),
+            selector: (old) => `${old} > *`
+          } 
+        }
+      }
+    ],
     rules: [
       [
         new RegExp(`^__container$`),
-        ([], { rawSelector, theme }: any) => {
-          const _selector = escapeSelector(rawSelector)
-          const _breakpoints = theme?.breakpoints ?? breakpoints
+        ([], { rawSelector, theme }) => {
+          const _selector = escapeSelector(rawSelector);
+          const _breakpoints = theme.breakpoints ?? breakpoints;
 
           const template = Object.keys(_breakpoints).map(
-            (breakpoint: string) => `@media (min-width: ${_breakpoints[breakpoint]}) {
+            (
+              breakpoint: string
+            ) => `@media (min-width: ${_breakpoints[breakpoint]}) {
               .${_selector} {
                 max-width: calc(${_breakpoints[breakpoint]} - ${piece});
               }
             }`
-          )
+          );
 
           return `
             .${_selector}, .${_selector}-fluid {
@@ -60,36 +71,35 @@ export function presetGrid(options: GridOptions = {}): Preset {
             }
             
             ${template.join("\n")}
-          `.replace(/^ {12}/gm, "")
-        }
+          `.replace(/^ {12}/gm, "");
+        },
       ],
       [
-        new RegExp(`^row$`),
-        ([], { rawSelector }) => {
-          const selector = escapeSelector(rawSelector)
-          const template = `
-            .${selector} {
-              --${prefix}gutter-x: ${gutter};
-              --${prefix}gutter-y: 0;
-            
-              display: flex;
-              flex-wrap: wrap;
-              
-              margin-top: calc(-1 * var(--${prefix}gutter-y));
-              margin-right: calc(-.5 * var(--${prefix}gutter-x));
-              margin-left: calc(-.5 * var(--${prefix}gutter-x));
-            }
-            .${selector} > * {
-              flex-shrink: 0;
-              width: 100%;
-              max-width: 100%;
-              padding-right: calc(var(--${prefix}gutter-x) * .5);
-              padding-left: calc(var(--${prefix}gutter-x) * .5);
-              margin-top: var(--${prefix}gutter-y);
-            }
-          `
-          return template.replace(/^ {12}/gm, "")
-        }
+        /^row$/,
+        ([], { rawSelector, constructCSS }) => {
+          const v1 = constructCSS({
+            [`--${prefix}gutter-x`]: gutter,
+            [`--${prefix}gutter-y`]: '0',
+  
+            "display": "flex",
+            "flex-wrap": "wrap",
+  
+            "margin-top": `calc(-1 * var(--${prefix}gutter-y))`,
+            "margin-right": `calc(-.5 * var(--${prefix}gutter-x))`,
+            "margin-left": `calc(-.5 * var(--${prefix}gutter-x))`,
+          }, rawSelector);
+
+          const v2 = constructCSS({
+            "flex-shrink": "0",
+            "width": "100%",
+            "max-width": "100%",
+            "padding-right": `calc(var(--${prefix}gutter-x) * .5)`,
+            "padding-left": `calc(var(--${prefix}gutter-x) * .5)`,
+            "margin-top": `var(--${prefix}gutter-y)`,
+          }, `${rawSelector}____`);
+
+          return v1 + '\n' + v2.replace(/_{4}/, '>*');
+        },
       ],
       [
         new RegExp(`^col-?(\\d*)$`),
@@ -97,46 +107,48 @@ export function presetGrid(options: GridOptions = {}): Preset {
           if (size) {
             return {
               flex: "0 0 auto",
-              width: `${(size / columns) * 100}%`
-            }
+              width: `${(size / columns) * 100}%`,
+            };
           } else {
             return {
-              flex: "1 0 0%"
-            }
+              flex: "1 0 0%",
+            };
           }
-        }
+        },
       ],
       [
         `col-auto`,
         {
           flex: "0 0 auto",
-          width: "auto"
-        }
+          width: "auto",
+        },
       ],
       [
         `col-fill`,
         {
           flex: "1 1 auto",
-          width: "auto"
-        }
+          width: "auto",
+        },
       ],
       [
         new RegExp(`^offset-(\\d+)$`),
         ([, size]: any): any => ({
-          "margin-left": size === "0" ? 0 : `${(size / columns) * 100}%`
-        })
+          "margin-left": size === "0" ? 0 : `${(size / columns) * 100}%`,
+        }),
       ],
       [
         new RegExp(`^g([xy])?-(\\d+)$`),
         ([, dim, size]: any): any => {
-          const gutterObject: { [key: string]: string } = {}
-          if (dim !== "y") gutterObject[`--${prefix}gutter-x`] = `${0.25 * size}rem`
-          if (dim !== "x") gutterObject[`--${prefix}gutter-y`] = `${0.25 * size}rem`
-          return gutterObject
-        }
-      ]
-    ]
-  }
+          const gutterObject: { [key: string]: string } = {};
+          if (dim !== "y")
+            gutterObject[`--${prefix}gutter-x`] = `${0.25 * size}rem`;
+          if (dim !== "x")
+            gutterObject[`--${prefix}gutter-y`] = `${0.25 * size}rem`;
+          return gutterObject;
+        },
+      ],
+    ],
+  };
 }
 
-export default presetGrid
+export default presetGrid;
--- unocss.config.ts
+++ unocss.config.ts
@@ -1,11 +1,13 @@
 import {
   defineConfig,
+  presetIcons,
+  presetTypography,
   presetUno,
   transformerCompileClass,
   transformerDirectives,
   transformerVariantGroup,
 } from "unocss";
-import { presetGrid } from './lib/unocss-preset-grid';
+import { presetGrid } from "./lib/unocss-preset-grid";
 import { createColorScaleGenerator } from "./scripts/color-scales-generate";
 
 const generateColorScale = createColorScaleGenerator(
@@ -17,19 +19,35 @@ export default defineConfig({
   // safelist: [...unoFiles.flatMap((data) => data.safelist ?? [])],
   safelist: [],
   rules: [
-    [/^bg@([\w-]+)$/, ([, name]) => ({ "background-color": `var(--h-color-${name})` })],
+    [
+      /^bg@([\w-]+)$/,
+      ([, name]) => ({ "background-color": `var(--h-color-${name})` }),
+    ],
     [/^text@([\w-]+)$/, ([, name]) => ({ color: `var(--h-color-${name})` })],
-    [/^border@([\w-]+)$/, ([, name]) => ({ "border-color": `var(--h-color-${name})` })],
+    [
+      /^border@([\w-]+)$/,
+      ([, name]) => ({ "border-color": `var(--h-color-${name})` }),
+    ],
+    [
+      /^outline@([\w-]+)$/,
+      ([, name]) => ({ "outline-color": `var(--h-color-${name})` }),
+    ],
   ],
+  presets: [presetGrid(), presetUno(), presetTypography()], //Container는 PresetUno 걸 이용할 것이므로 순서 유의!
   theme: {
     colors: {
       transprent: "transparent",
       current: "currentColor",
-      primary: { ...generateColorScale("orange") },
-      neutral: { ...generateColorScale("sand") },
+      primary: { ...generateColorScale("teal") },
+      neutral: { ...generateColorScale("sage") },
     },
     container: {
       center: true,
+      padding: {
+        sm: "16px",
+        md: "16px",
+        lg: "24px",
+      },
     },
     breakpoints: {
       sm: "672px", //640px
@@ -37,7 +55,6 @@ export default defineConfig({
       lg: "1176px", //1128px
     },
   },
-  presets: [presetUno(), presetGrid()],
   transformers: [
     transformerCompileClass(),
     transformerVariantGroup(),
